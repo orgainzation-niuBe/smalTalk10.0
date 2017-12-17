b2bfc6145900e0cd73a284d718b2bc8abbee1fb0
FreeBaiShun <851083075@qq.com>
2017-12-17 19:35:12 +0800

    1

---
+++ smoreTalk/KuangJiaViewController.h
@@ -0,0 +1,13 @@
+//
+//  KuangJiaViewController.h
+//  smoreTalk
+//
+//  Created by Mac on 2017/12/17.
+//  Copyright © 2017年 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KuangJiaViewController : UIViewController
+
+@end
---
+++ smoreTalk/KuangJiaViewController.m
@@ -0,0 +1,37 @@
+//
+//  KuangJiaViewController.m
+//  smoreTalk
+//
+//  Created by Mac on 2017/12/17.
+//  Copyright © 2017年 1. All rights reserved.
+//
+
+#import "KuangJiaViewController.h"
+
+@interface KuangJiaViewController ()
+
+@end
+
+@implementation KuangJiaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
